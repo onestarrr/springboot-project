fe24ba30b804239526c8e75a0938a270c05693e9
onestarrr <johb1128@gmail.com>
2022-01-19 17:07:11 +0900

    게시글 추가, TimeZone 설정

---
+++ src/main/java/com/project/springbootproject/dto/BoardDto.java
@@ -0,0 +1,40 @@
+package com.project.springbootproject.dto;
+
+import com.project.springbootproject.domain.entity.BoardEntity;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class BoardDto {
+    private Long id;
+    private String writer;
+    private String title;
+    private String content;
+    private LocalDateTime createdDate;
+    private LocalDateTime modifiedDate;
+
+    public BoardEntity toEntity() {
+        BoardEntity boardEntity = BoardEntity.builder()
+                .id(id)
+                .writer(writer)
+                .title(title)
+                .content(content)
+                .build();
+            return boardEntity;
+    }
+
+    @Builder
+    public BoardDto(Long id, String title, String content, String writer, LocalDateTime createdDate, LocalDateTime modifiedDate) {
+        this.id = id;
+        this.title = title;
+        this.content = content;
+        this.writer = writer;
+        this.createdDate = createdDate;
+        this.modifiedDate = modifiedDate;
+    }
+
+}
---
+++ src/main/java/com/project/springbootproject/service/BoardService.java
@@ -0,0 +1,19 @@
+package com.project.springbootproject.service;
+
+import com.project.springbootproject.domain.repository.BoardRepository;
+import com.project.springbootproject.dto.BoardDto;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@AllArgsConstructor
+@Service
+public class BoardService {
+    private BoardRepository boardRepository;
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+}
